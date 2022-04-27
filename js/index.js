724adf09c6ada9fc46214d1e5619d2b370069969
Joshua Cobos <satochiotake@gmail.com>
2022-04-26 21:07:19 -0500

    added the proyect section

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ let spanish=document.getElementsByClassName('es')
 let english=document.getElementsByClassName('en')
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('hidden-skills').style.display = "none";
     for(var i = 0; i < spanish.length; i++) {
         spanish[i].style.display = "none";
         english[i].style.display = "block";
@@ -24,5 +25,14 @@ function selectLan(el) {
             spanish[i].style.display = "block";
             english[i].style.display = "none";
         }
+    }
+}
+
+function showSkills(){
+    const skills = document.getElementById('hidden-skills');
+    if (skills.style.display === "none") {
+        skills.style.display = "block";
+    } else {
+        skills.style.display = "none";
     }
 }
