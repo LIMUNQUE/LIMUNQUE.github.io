b74fa3af5f979471ae8186cbe0087de7f169b23e
Joshua Cobos <satochiotake@gmail.com>
2022-05-04 22:23:40 -0500

    added the loader & the contact section

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
-//let language = window.navigator.userLanguage || window.navigator.language;
+/**********************************************/
+/*  Loader
+/**********************************************/
+let section = document.getElementsByTagName("section")
+let loader_ctn = document.getElementById("loader_ctn")
+window.addEventListener("load", () => {
+    for (let i = 0; i < section.length; i++) {
+        section[i].classList.remove("hidden");
+    }
+    loader_ctn.classList.add("hidden");
+})
+
 //Language Selection Function
 
-let spanish=document.getElementsByClassName('es')
-let english=document.getElementsByClassName('en')
+let spanish = document.getElementsByClassName('es')
+let english = document.getElementsByClassName('en')
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById('hidden-skills').style.display = "none";
-    for(var i = 0; i < spanish.length; i++) {
+    for (var i = 0; i < spanish.length; i++) {
         spanish[i].style.display = "none";
         english[i].style.display = "block";
     }
@@ -14,21 +25,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function selectLan(el) {
     lang = el.selectedIndex
-    
+
     if (lang == 0) {
-        for(var i = 0; i < spanish.length; i++) {
+        for (var i = 0; i < spanish.length; i++) {
             spanish[i].style.display = "none";
             english[i].style.display = "block";
         }
-    }else{
-        for(var i = 0; i < spanish.length; i++) {
+    } else {
+        for (var i = 0; i < spanish.length; i++) {
             spanish[i].style.display = "block";
             english[i].style.display = "none";
         }
     }
 }
 
-function showSkills(){
+function showSkills() {
     const skills = document.getElementById('hidden-skills');
     if (skills.style.display === "none") {
         skills.style.display = "block";
